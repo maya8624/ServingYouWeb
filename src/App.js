ead884707790f1e5c13fe13dcdd1225e7554ad1e
Joseph <maya8624@hotmail.com>
2020-12-04 10:36:34 +1100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
+
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Order from "./pages/Order";
+import MenuList from "./components/MenuList";
+import BookingForm from "./components/BookingForm";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About} />
+        <Route exact path="/menuList" component={MenuList}></Route>
+        <Route path="/orrder" component={Order}></Route>
+        <Route path="/booking" component={BookingForm}></Route>
+        <Route path="/login" component={LoginForm}></Route>
+        <Route path="/register" component={RegisterForm}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+    </>
   );
 }
-
 export default App;
