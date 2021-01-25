44a171d5c031d036de35e4c82f5d3eacf8f6dfdb
Joseph <maya8624@hotmail.com>
2021-01-26 10:45:14 +1100

    change OrderContext2 -> OrderContext in useOrder

--- src/components/hooks/useOrder.js
+++ src/components/hooks/useOrder.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { OrderContext } from "../../context/OrderContext2";
+import { OrderContext } from "../../context/OrderContext";
 
 export const useOrder = () => {
   return useContext(OrderContext);
